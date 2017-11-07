578d4bfa88c2e8a31d7f0aadcd14bf1889b7bf9d
richardFirth <richardfirthucsb@gmail.com>
2017-11-07 15:49:05 -0800

    Add files via upload

---
+++ Balancing_robot_RF3/AD_ControlCalc.ino
@@ -0,0 +1,37 @@
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //Control calculations
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+void controlCalculation()
+ { 
+  
+  pid_output_left = pid_output;                                             //Copy the controller output to the pid_output_left variable for the left motor
+  pid_output_right = pid_output;                                            //Copy the controller output to the pid_output_right variable for the right motor
+
+  if(R_TRIG){                                            //If the first bit of the receive byte is set change the left and right variable to turn the robot to the left
+    pid_output_left += myOperatingValues.turning_speed;                                       //Increase the left motor speed
+    pid_output_right -= myOperatingValues.turning_speed;                                      //Decrease the right motor speed
+  } else
+  if(L_TRIG){                                            //If the second bit of the receive byte is set change the left and right variable to turn the robot to the right
+    pid_output_left -= myOperatingValues.turning_speed;                                       //Decrease the left motor speed
+    pid_output_right += myOperatingValues.turning_speed;                                      //Increase the right motor speed
+  } else
+  if(DOWN_BUTTON){                                            //If the third bit of the receive byte is set change the left and right variable to turn the robot to the right
+    if(pid_setpoint > -2.5)pid_setpoint -= 0.05;                            //Slowly change the setpoint angle so the robot starts leaning forewards
+    if(pid_output > myOperatingValues.max_target_speed * -1)pid_setpoint -= 0.005;            //Slowly change the setpoint angle so the robot starts leaning forewards
+  } else
+  if(RIGHT_BUTTON){                                            //If the forth bit of the receive byte is set change the left and right variable to turn the robot to the right
+    if(pid_setpoint < 2.5)pid_setpoint += 0.05;                             //Slowly change the setpoint angle so the robot starts leaning backwards
+    if(pid_output < myOperatingValues.max_target_speed)pid_setpoint += 0.005;                 //Slowly change the setpoint angle so the robot starts leaning backwards
+  } else {                                                    //Slowly reduce the setpoint to zero if no foreward or backward command is given
+    if(pid_setpoint > 0.5)pid_setpoint -=0.05;                              //If the PID setpoint is larger then 0.5 reduce the setpoint with 0.05 every loop
+    else if(pid_setpoint < -0.5)pid_setpoint +=0.05;                        //If the PID setpoint is smaller then -0.5 increase the setpoint with 0.05 every loop
+    else pid_setpoint = 0;                                                  //If the PID setpoint is smaller then 0.5 or larger then -0.5 set the setpoint to 0
+  }
+  
+  //The self balancing point is adjusted when there is not forward or backwards movement from the transmitter. This way the robot will always find it's balancing point
+  if(pid_setpoint == 0){                                                    //If the setpoint is zero degrees
+    if(pid_output < 0)self_balance_pid_setpoint += 0.0015;                  //Increase the self_balance_pid_setpoint if the robot is still moving forewards
+    if(pid_output > 0)self_balance_pid_setpoint -= 0.0015;                  //Decrease the self_balance_pid_setpoint if the robot is still moving backwards
+  }
+ }
---
+++ Balancing_robot_RF3/ZZ_Misc.ino
@@ -0,0 +1,42 @@
+
+
+
+void loadOperatingValues()
+{
+  EEPROM.get(0, myOperatingValues);
+}
+
+
+
+void resetDefaultValues()
+{
+  // hold down pushbutton on start to do this.
+}
+
+
+
+
+
+
+
+
+void defaultOperatingValues()
+{
+  myOperatingValues.acc_calibration_value = 1300;                            //Enter the accelerometer calibration value (1182)
+  myOperatingValues.pid_p_gain = 10;                                       //Gain setting for the P-controller (15)
+  myOperatingValues.pid_i_gain = 1.5;                                      //Gain setting for the I-controller (1.5)
+  myOperatingValues.pid_d_gain = 30;                                       //Gain setting for the D-controller (30)
+  myOperatingValues.turning_speed = 30;                                    //Turning speed (20)
+  myOperatingValues.max_target_speed = 150;                                //Max target speed (100)
+  EEPROM.put(0, myOperatingValues);
+}
+
+
+int getBalanceValue()
+{
+    Wire.beginTransmission(0x68);
+    Wire.write(0x3F);
+    Wire.endTransmission();
+    Wire.requestFrom(0x68,2);
+    return ((Wire.read()<<8|Wire.read())*-1);
+}
